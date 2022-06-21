8b1a40087776c701b9ba3156e20de9d5b191f52e
Stieilijh <saitushar02@gmail.com>
2022-06-21 20:17:57 +0530

    https

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ window.onload = () => {
     let APIkey = "328d9347e31008ec5c5155528274de7b";
     let dataObj;
     const data = fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${APIkey}`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${APIkey}`,
       { mode: "cors" }
     )
       .then((response) => {
